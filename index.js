93c70de88ee269b9c5edb778d0ef9f319856e13a
Sarthak Patil <sarthak05patil@gmail.com>
2025-04-11 15:08:55 +0530

    New Commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
